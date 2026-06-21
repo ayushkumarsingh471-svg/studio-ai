aa24cdc59775341153667f4ff2f464911b0c9559
Ayush Singh <ayushkumarsingh471@gmail.com>
2026-06-21 13:14:39 +0530

    Fixed Stripe version in webhook file

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,7 +7,7 @@ import { eq } from "drizzle-orm";
 
 // Stripe Engine Start
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-04-10",
+  apiVersion: "2026-05-27.dahlia",
 });
 
 export async function POST(req: Request) {
