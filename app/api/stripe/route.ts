5a041aef85ca22ee9dae7e780279a2af12d18460
Ayush Singh <ayushkumarsingh471@gmail.com>
2026-06-21 13:00:53 +0530

    Fixed Stripe API version

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -15,7 +15,7 @@ export async function GET() {
     }
 
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: "2024-04-10",
+      apiVersion: "2026-05-27",
     });
 
     // 2. User Login Check
